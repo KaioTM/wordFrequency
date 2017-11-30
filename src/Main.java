95e63e2e94df2fa7ef21ea7bec675faedae75496
Kaio Teixeira <kaioteixeira@id.uff.br>
2017-11-29 22:42:27 -0200

    teste

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,7 @@ import br.com.uff.wordFrequency.model.ContaPalavras;
 
 public class Main {
 
-	public static void main(String[] args) throws Exception {
+	public static void main(String[] args) throws IOException {
 		//String texto = TextoDAO.processarArquivo("C:\\Users\\kaio.teixeira\\git\\wordFrequency\\src\\arquivo\\Comentarios.txt");
 		ContaPalavras contar = new ContaPalavras();
 		contar.frequenciaPalavras();
